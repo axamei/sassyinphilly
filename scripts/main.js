e17410a4b20fe675204da200a4baac99a3caa928
Axa Mei Liauw <aliauw@princeton.edu>
2016-02-04 20:59:43 -0500

    Update 2016-02-05T01:59:43.099Z

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){var e=document.querySelectorAll(".dropcap");window.Dropcap.layout(e,2);responsiveNav(".nav-collapse");$(".time").text(function(e,o){return Math.round(parseFloat(o))})});
+//# sourceMappingURL=main.js.map
